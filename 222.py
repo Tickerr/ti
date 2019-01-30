3fc91dd42e2926d9b92989121e01547bcc75145c
Tickerr <43993153+Tickerr@users.noreply.github.com>
2019-01-30 12:10:22 +0100

    Add files via upload

---
+++ 222.py
@@ -0,0 +1,17 @@
+#!/bin/env python3
+def nowe_a():
+    return a-b
+def nowe_b():
+    return b-a
+
+a = int(input("Podaj a: "))
+b = int(input("Podaj b: "))
+
+if a==b:
+    print (a)
+elif a>b:
+    nowe_a
+else:
+    nowe_b
+    
+    
